f8142d669d243d91a89ba739ac7ca454482adfba
wnjoon <wnjoon@gmail.com>
2025-08-27 13:07:07 +0900

    fix MockERC20.t.sol

--- 1a/src/MockERC20.sol
+++ 1a/src/MockERC20.sol
@@ -7,11 +7,22 @@ import {IMockERC20} from "./IMockERC20.sol";
 // Add as many variables or functions as you would like
 // for the implementation. The goal is to pass `forge test`.
 contract MockERC20 is ERC20, IMockERC20 {
+    // since ERC20 is already a contract, we can use it as a base contract
+    // name, symbol, decimals are already defined in ERC20
     constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {}
 
     // Implement
-    function freeMintTo(uint256 amount, address to) external {}
+    // use ERC20 _mint function directly
+    function freeMintTo(uint256 amount, address to) external {
+        require(msg.sender != address(0), "sender cannot be zero address");
+        _mint(to, amount);
+    }
 
     // Implement
-    function freeMintToSender(uint256 amount) external {}
+    // use ERC20 _mint function directly
+    function freeMintToSender(uint256 amount) external {
+        // msg.sender is the address of the caller
+        require(msg.sender != address(0), "sender cannot be zero address");
+        _mint(msg.sender, amount);
+    }
 }
