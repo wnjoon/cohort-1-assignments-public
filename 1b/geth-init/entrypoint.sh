a3b1f0d4f8f6fc4683726693a7e7197f791fc995
9oelM <hj923@hotmail.com>
2025-08-30 22:32:28 +0900

    feat: sc-deployer & geth-init

--- 1b/geth-init/entrypoint.sh
+++ 1b/geth-init/entrypoint.sh
@@ -2,14 +2,14 @@
 
 apk update && apk add curl
 
-until curl -s http://ethereum:8545 >/dev/null; do
+until curl -s http://geth:8545 >/dev/null; do
   echo "Waiting for geth JSON-RPC to respond..."
   sleep 0.5
 done
 
 echo "Geth JSON-RPC is responding!"
 
-geth attach --exec 'loadScript("/prefund.js")' http://ethereum:8545
+geth attach --exec 'loadScript("/scripts/prefund.js")' http://geth:8545
 
 echo "Finished prefunding the account."
 
