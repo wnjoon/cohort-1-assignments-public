8c9a9eec9484280bf17862ebb2aac8695f1179ff
wnjoon <wnjoon@gmail.com>
2025-09-09 06:10:43 +0900

    add scripts

--- 1b/run-blockscout.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-BLOCKSCOUTPATH=blockscout/docker-compose
-docker-compose -f $BLOCKSCOUTPATH/geth.yml up -d
--- 1b/stop-blockscout.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-BLOCKSCOUTPATH=blockscout/docker-compose
-docker-compose -f $BLOCKSCOUTPATH/geth.yml down -v
-
-rm -rf $BLOCKSCOUTPATH/services/blockscout-db-data
