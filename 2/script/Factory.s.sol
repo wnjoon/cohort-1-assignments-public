a90ce5814b315d7017233e7ec1f4b2dfbff728cc
wnjoon <wnjoon@gmail.com>
2025-09-10 13:19:53 +0900

    update foundry to handle paris

--- 2/script/Factory.s.sol
+++ 2/script/Factory.s.sol
@@ -18,11 +18,19 @@ contract FactoryScript is Script {
         vm.startBroadcast();
 
         // Step 1: Deploy MiniAMMFactory
+        miniAMMFactory = new MiniAMMFactory();
 
         // Step 2: Deploy two MockERC20 tokens
+        token0 = new MockERC20("Token A", "TKA");
+        token1 = new MockERC20("Token B", "TKB");
 
         // Step 3: Create a MiniAMM pair using the factory
+        pair = miniAMMFactory.createPair(address(token0), address(token1));
 
         vm.stopBroadcast();
     }
 }
+
+// 0xfc5726b3ad9f313d8e7cf0cbf8fd4df9a7c2261a
+// forge script script/Factory.s.sol --slow --rpc-url https://coston2-api.flare.network/ext/C/rpc --account hm-deployer --broadcast 
+// forge script script/Factory.s.sol --slow --rpc-url https://coston2-api.flare.network/ext/C/rpc --account 0xfc5726b3ad9f313d8e7cf0cbf8fd4df9a7c2261a --broadcast
